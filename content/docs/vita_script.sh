9673febbce54d9bc3c8687cf764e8051e265b4fd
Travis <travis@travis-ci.org>
2018-11-09 09:04:10 -0800

    modified vita_script.sh and added new CV to output/docs directory

--- content/docs/vita_script.sh
+++ content/docs/vita_script.sh
@@ -1,2 +1,2 @@
 cp ../../../vita/Polimis_Curriculum_Vitae_Industry.pdf Polimis_Curriculum_Vitae.pdf
-cp Polimis_Curriculum_Vitae.pdf ../../output/.
+cp Polimis_Curriculum_Vitae.pdf ../../output/docs/

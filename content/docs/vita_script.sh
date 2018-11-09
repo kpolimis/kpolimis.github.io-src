8e6a6796ea08434ec16d6f6e4782771576d451b6
Travis <travis@travis-ci.org>
2018-11-09 09:06:37 -0800

    modified vita_script.sh

--- content/docs/vita_script.sh
+++ content/docs/vita_script.sh
@@ -1,2 +1,2 @@
 cp ../../../vita/Polimis_Curriculum_Vitae_Industry.pdf Polimis_Curriculum_Vitae.pdf
-cp Polimis_Curriculum_Vitae.pdf ../../output/docs/
+cp Polimis_Curriculum_Vitae.pdf ../../output/docs/.

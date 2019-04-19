df7fe1290d306905fb3a60506813d66418025200
kpolimis <kivan.polimis@gmail.com>
2019-04-18 21:07:16 -0500

    modified vita page and CV/Resume on website

--- content/docs/vita_script.sh
+++ content/docs/vita_script.sh
@@ -1,2 +1,6 @@
-cp ../../../vita/Polimis_Curriculum_Vitae_Industry.pdf Polimis_Curriculum_Vitae.pdf
-cp Polimis_Curriculum_Vitae.pdf ../../output/docs/.
+cp ../../../vita/Polimis_Curriculum_Vitae.pdf Kivan_Polimis_Curriculum_Vitae.pdf
+cp Kivan_Polimis_Curriculum_Vitae.pdf ../../output/docs/.
+
+
+cp ../../../vita/Polimis_Curriculum_Vitae_Industry.pdf Kivan_Polimis_Resume.pdf
+cp Kivan_Polimis_Resume.pdf ../../output/docs/.
--- content/pages/vita.md
+++ content/pages/vita.md
@@ -2,6 +2,7 @@
 Title: Vita
 sortorder: 002
 ---
-[Kivan Polimis CV.pdf](../../docs/Polimis_Curriculum_Vitae.pdf)  
+[Kivan Polimis Curriculum Vitae.pdf](../../docs/Kivan_Polimis_Curriculum_Vitae.pdf)  
+[Kivan Polimis Resume.pdf](../../docs/Kivan_Polimis_Resume.pdf)  
 [Software Carpentry](../../docs/Software_Carpentry_Certification.pdf)  
 [Data Carpentry](../../docs/Data_Carpentry_Certification.pdf)

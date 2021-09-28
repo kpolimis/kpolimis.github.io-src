f8361213cfa3cc07e697f2a0b56c8da2bcf582c1
Travis <travis@travis-ci.org>
2021-09-28 12:44:00 -0500

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ PELICAN_OUTPUT_FOLDER=output
 
 echo -e "Testing travis-encrypt"
 echo -e "$VARNAME"
+echo -e "${GH_TOKEN}"
 
 if ["$TRAVIS_PULL_REQUEST" =="false" ]; then
     echo -e "Starting to deploy to Github Pages\n"
@@ -13,9 +14,8 @@ if ["$TRAVIS_PULL_REQUEST" =="false" ]; then
         git config user.name "Travis"
     fi
     #using token clone gh-pages branch
-    git clone --depth 1 --quiet --branch=$BRANCH https://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@github.com/$TARGET_REPO built_website > /dev/null
-#    git remote remove origin
-#    git remote add origin	https://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@github.com/kpolimis/kpolimis.github.io-src.git
+#    git clone --depth 1 --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
+    git clone --depth 1 --quiet --branch=master https://github.com/kpolimis/$TARGET_REPO built_website > /dev/null
     #go into directory and copy data we're interested in to that directory
     cd built_website
     rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* .

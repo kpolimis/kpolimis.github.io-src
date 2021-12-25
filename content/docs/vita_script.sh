e01df790eec1c46809a077870023fda72449816d
Travis <travis@travis-ci.org>
2021-12-25 17:37:57 -0600

    changed vita script and updated .html files

--- content/docs/vita_script.sh
+++ content/docs/vita_script.sh
@@ -1,6 +1,6 @@
-cp ../../../vita/Polimis_Curriculum_Vitae.pdf Kivan_Polimis_Curriculum_Vitae.pdf
+cp ~/repos/vita/Polimis_Curriculum_Vitae.pdf Kivan_Polimis_Curriculum_Vitae.pdf
 cp Kivan_Polimis_Curriculum_Vitae.pdf ../../output/docs/.
 
 
-cp ../../../vita/Polimis_Curriculum_Vitae_Industry.pdf Kivan_Polimis_Resume.pdf
+cp ~/repos/vita/Polimis_Curriculum_Vitae_Industry.pdf Kivan_Polimis_Resume.pdf
 cp Kivan_Polimis_Resume.pdf ../../output/docs/.
